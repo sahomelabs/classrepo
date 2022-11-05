c9cfbadd96e6f3f942aa9aaf904a61f21760dfa5
unknown <sahomelabs@gmail.com>
2022-11-04 22:02:52 -0400

    My nodejs push

---
+++ nodejs-jenkins/main.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express()
+
+//Define request response in root URL (/)
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+app.listen(8080, function () {
+  console.log('app listening on port 8080!')
+})
